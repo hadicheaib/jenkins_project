0e83da4a9b2dd6dd69db55f4b9f371ec7b69ae73
Hadi Cheaib <hadi.cheaib9763@gmail.com>
2025-11-09 17:46:04 +0200

    Add unit test and fix flake8 issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-def greet(name):
-    return f"Hello, {name}!"
+def greet(name: str) -> str:
+    return f"Hello, {name} from FirstName LastName!"
+
 
 if __name__ == "__main__":
     print(greet("World"))
